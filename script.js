874b08062b295a739fbfb7665035833cb9e105b1
Rathin21 <114067851+Rathin21@users.noreply.github.com>
2023-02-10 17:45:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
 
+function GenerateRandom(){
+	let x=Math.floor((Math.random()*20)+1);
+	// document.getElementById("num").innerHTML=x;
+	let guessedValue=document.getElementById("guess").value;
+	if(Math.abs(x-guessedValue)==0)
+	{
+		document.getElementById("num").innerHTML="Hot";
+	}
+	else{
+		document.getElementById("num").innerHTML="Cold";
+	}
+}
